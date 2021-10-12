90339151bc0fc2e0acbd8f1b4be3bedb9248781b
Fabian Reiterer <reiterer@gmx.net>
2021-10-12 12:19:42 +0200

    started main menu creation

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer.material.color = Color.black;
+    }
+
+    void OnMouseEnter()
+    {
+        renderer.material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.black;
+    }
+}
